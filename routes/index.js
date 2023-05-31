6446bb9054ccec56fe826644489b7a04e74765fe
dongseob230316 <sorrow3148@naver.com>
2023-05-31 23:35:35 +0900

    댓글 입력 삭제

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const authosRouter = require("./auth");
 const mainlist_router = require("./mainlist");
+const videoplay_router = require("./videoplay");
 //const todosRouter = require("./todos");
 
-module.exports = [authosRouter, mainlist_router];
+module.exports = [authosRouter, mainlist_router, videoplay_router];
--- routes/videoplay.js
+++ routes/videoplay.js
@@ -3,6 +3,7 @@ const videoplay_router = express.Router();
 require("dotenv").config();
 const env = process.env;
 const { Comment } = require("../models");
+const authMiddleware = require("../middlewares/auth-middleware");
 
 //영상정보
 videoplay_router.get("/api/videoinfo/:id", async (req, res, next) => {
@@ -29,13 +30,87 @@ videoplay_router.get("/api/:id/comment", async (req, res, next) => {
 });
 
 //댓글입력
-videoplay_router.post("/api/:id/comment", async (req, res, next) => {
-  console.log("댓글 입력 API 호출됨");
-  return res.status(200);
-});
+videoplay_router.post(
+  "/:id/comment",
+  authMiddleware,
+  async (req, res, next) => {
+    try {
+      console.log(req.body);
+      const MovieId = req.params.id;
+      const { commentText } = req.body;
+      const userId = res.locals.user.UserId;
+
+      console.log(MovieId);
+      console.log(commentText);
+
+      if (!MovieId || MovieId.length === 0) {
+        res.status(404).json({ errorMessage: "MovieId 를 찾을수 없습니다." });
+        return;
+      }
+
+      if (!commentText || commentText === "") {
+        res.status(404).json({ errorMessage: "comment 를 입력해 주세요." });
+        return;
+      }
+
+      const createadComment = await Comment.create({
+        MovieId,
+        UserId: userId,
+        Comment: commentText,
+        // CommentId: createadComment.id,
+      });
+      console.log(createadComment);
+      res.status(200).json({ message: "댓글이 성공적으로 작성되었습니다." });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ errorMessage: "댓글 입력을 실패하였습니다." });
+    }
+  }
+);
 
 //댓글삭제
-videoplay_router.delete("/api/:movieid/comment/:id", async (req, res, next) => {
-  console.log("댓글 삭제 API 호출됨");
-  return res.status(200);
-});
+videoplay_router.delete(
+  "/movie/:id/comment/:commentId",
+  authMiddleware,
+  async (req, res, next) => {
+    try {
+      const MovieId = req.params.id;
+      const commentId = req.params.commentId;
+
+      console.log(MovieId);
+      console.log(commentId);
+
+      if (!MovieId || MovieId.length === 0) {
+        res.status(404).json({ errorMessage: "영상 ID를 찾을 수 없습니다." });
+        return;
+      }
+
+      if (!commentId || commentId.length === 0) {
+        res.status(404).json({ errorMessage: "댓글 ID를 찾을 수 없습니다." });
+        return;
+      }
+
+      // 댓글 조회 후 삭제
+      const comment = await Comment.findOne({
+        where: { MovieId: MovieId, id: commentId },
+      });
+      console.log(comment);
+
+      if (!comment) {
+        res
+          .status(404)
+          .json({ errorMessage: "삭제할 댓글을 찾을 수 없습니다." });
+        return;
+      }
+
+      await comment.destroy();
+
+      res.status(200).json({ message: "댓글이 성공적으로 삭제되었습니다." });
+    } catch (error) {
+      console.error("댓글 삭제 실패:", error);
+      res.status(500).json({ error: "댓글 삭제에 실패했습니다." });
+    }
+  }
+);
+
+module.exports = videoplay_router;

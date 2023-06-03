642b96d2d508e494e3ecae7b5473af7d628e7973
dongseob230316 <sorrow3148@naver.com>
2023-06-03 14:03:12 +0900

    test

--- models/videolist.js
+++ models/videolist.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class VideoList extends Model {
     /**
@@ -13,16 +11,19 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  VideoList.init({
-    UserId: DataTypes.STRING,
-    MovieId: DataTypes.STRING,
-    Title: DataTypes.STRING,
-    Like: DataTypes.INTEGER,
-    View: DataTypes.INTEGER,
-    URL: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'VideoList',
-  });
+  VideoList.init(
+    {
+      UserId: DataTypes.STRING,
+      MovieId: DataTypes.STRING,
+      Title: DataTypes.STRING,
+      Like: DataTypes.INTEGER,
+      View: DataTypes.INTEGER,
+      URL: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "VideoList",
+    }
+  );
   return VideoList;
 };
